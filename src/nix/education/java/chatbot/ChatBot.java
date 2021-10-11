6723b706c9dca10dd2276c5af908733c6102dc10
i.strokan <i.strokan@student.csn.khai.edu>
2021-10-11 18:54:13 +0300

    ChatBot 1-st stage

---
+++ src/nix/education/java/chatbot/ChatBot.java
@@ -0,0 +1,8 @@
+package nix.education.java.chatbot;
+
+public class ChatBot {
+    public static void main(String args[]){
+        System.out.println("Hello! My name is STR");
+        System.out.println("I was created in 2021");
+    }
+}
